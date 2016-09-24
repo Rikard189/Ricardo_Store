0d80adf68632f5c9dc25842cf0f33e15795668cc
Ricardo Acosta <rikard189@gmail.com>
2016-09-24 15:49:21 -0500

    Commit devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_categories
+  before_action :set_product_type
 
   private
 
   def set_categories
     @categories = Category.all
+  end
+
+  def set_product_type
+    @product_types = ProductType.all
   end
 end
